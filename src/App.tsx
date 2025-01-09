28df593a4552684de7075a144ebd8f0dc066edd9
Danielson <danielson7632@gmail.com>
2025-01-09 10:05:50 +0700

    complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
 import './App.css'
+import Counter from './components/Counter'
 
-//Store 
+//Store  Global State
+interface CounterState{
+  value: number
+}
+
+interface USerState{
+  isSignedIn: boolean
+}
+
+//Action  Action to state
+const increment = {type: "INCREMENT"}
+// const incrementByAmount = {type: "INCREMENT", payload: 10}
+const decrement = {type: "DECREMENT"}
+
+//Reducers  Taking Action depend on type and make update 
+
 
 function App() {
   return (
     <>
       <h2>Redux Tutorial</h2>
+      <Counter />
     </>
   )
 }
